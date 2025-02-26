d5a46a42e69a5cc6cb66367c306d07bd38302703
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2025-02-26 11:13:26 +1100

    Set refresh to true

--- src/main/kotlin/app/krail/kgtfs/Main.kt
+++ src/main/kotlin/app/krail/kgtfs/Main.kt
@@ -10,7 +10,7 @@ fun main() {
     println("Welcome to KRAIL GTFS")
 
     runBlocking {
-        NswGtfsManager.fetch(refresh = false)
+        NswGtfsManager.fetch(refresh = true)
             .let(::filterOutBusStandData)
             .let {
                 writeStopData(it)
